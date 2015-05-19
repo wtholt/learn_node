22dded209a56af4ddb1c9c5dce81e6f338d6aa4f
William Holt <wtholt27@gmail.com>
2015-05-19 15:14:57 -0400

    learnyounode

--- juggling_async.js
+++ juggling_async.js
@@ -1,8 +1,26 @@
 var http = require('http')
-var url = process.argv[2]
+var bl = require('bl')
+var results = []
+var count = 0
 
-http.get(url, function callback(response){
-  response.setEncoding('utf8')
-  response.on('data', console.log)
-  response.on('error', console.error)
-});
+function printResults () {
+  for (var i = 0; i < 3; i++)
+    console.log(results[i])
+}
+
+function httpGet (index) {
+  http.get(process.argv[2 + index], function (response){
+    response.pipe(bl(function (err, data){
+      if (err)
+        return console.error(err)
+      results[index] = data.toString()
+      count++
+
+      if (count == 3)
+        printResults()
+    }))
+  })
+}
+
+for (var i = 0; i < 3; i++)
+  httpGet(i)
